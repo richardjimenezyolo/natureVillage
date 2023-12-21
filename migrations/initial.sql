7211cf1ad433489236d0e580ec18e23d0901a334
Richard <richardjimenez176@gmail.com>
2023-12-20 22:32:39 -0400

    Crear una tabla para guardar de manera indexada la informacion del view

--- migrations/initial.sql
+++ migrations/initial.sql
@@ -6,14 +6,14 @@ CREATE TABLE `houses_raw` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=533 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE OR REPLACE ALGORITHM = UNDEFINED VIEW `house` AS with cte as (
+CREATE VIEW house as with `cte` as (
 select
     regexp_replace(`hr`.`topic`, 'Nature_Village/HOUSES/', '') AS `topic`,
     `hr`.`message` AS `message`,
     `hr`.`created_at` AS `created_at`
 from
     `houses_raw` `hr`),
-parsed as (
+`parsed` as (
 select
     regexp_replace(`cte`.`topic`, '/.*$', '') AS `house`,
     regexp_replace(`cte`.`topic`, '.*/', '') AS `metric`,
@@ -21,21 +21,20 @@ select
     `cte`.`created_at` AS `created_at`
 from
     `cte`),
-parsed_data_value as (
+`parsed_data_value` as (
 select
     `parsed`.`house` AS `house`,
     `parsed`.`metric` AS `metric`,
     `parsed`.`message` AS `message`,
     `parsed`.`created_at` AS `created_at`,
-    lead(`parsed`.`message`, 1) over (
-order by
-    `parsed`.`metric`) AS `value`
+    lead(`parsed`.`message`, 1) OVER (
+    ORDER BY `parsed`.`metric` ) AS `value`
 from
     `parsed`
 where
-    1 = 1
-    and `parsed`.`metric` = 'data'),
-joined_parse as (
+    ((1 = 1)
+        and (`parsed`.`metric` = 'data'))),
+`joined_parse` as (
 select
     `parsed_data_value`.`house` AS `house`,
     `parsed_data_value`.`metric` AS `metric`,
@@ -45,7 +44,7 @@ select
 from
     `parsed_data_value`
 where
-    `parsed_data_value`.`message` like '>%'
+    (`parsed_data_value`.`message` like '>%')
 union all
 select
     `parsed`.`house` AS `house`,
@@ -56,17 +55,42 @@ select
 from
     `parsed`
 where
-    `parsed`.`metric` <> 'data'
-)select
+    (`parsed`.`metric` <> 'data'))
+select
+ROW_NUMBER() over (PARTITION by house)as id,
     `joined_parse`.`house` AS `house`,
-    max(if(`joined_parse`.`metric` = 'WATER_METER', `joined_parse`.`message`, 0)) AS `water_gal`,
-    max(if(`joined_parse`.`metric` = 'ENERGY_METER', `joined_parse`.`message`, 0)) AS `energy_wh`,
-    max(if(`joined_parse`.`message` like '%wi%fi%' and `joined_parse`.`value` regexp '[0-9]]*', `joined_parse`.`value`, NULL)) AS `wifi_dbm`,
-    max(if(`joined_parse`.`message` like '%electrovalvula%', `joined_parse`.`value`, NULL)) AS `electrovalvula`,
-    max(if(`joined_parse`.`message` like '%contactor%', `joined_parse`.`value`, NULL)) AS `contactor`,
+    max(if((`joined_parse`.`metric` = 'WATER_METER'), `joined_parse`.`message`, 0)) AS `water_gal`,
+    max(if((`joined_parse`.`metric` = 'ENERGY_METER'), `joined_parse`.`message`, 0)) AS `energy_wh`,
+    max(if(((`joined_parse`.`message` like '%wi%fi%') and regexp_like(`joined_parse`.`value`, '[0-9]]*')), `joined_parse`.`value`, NULL)) AS `wifi_dbm`,
+    max(if((`joined_parse`.`message` like '%electrovalvula%' and `joined_parse`.`value` not REGEXP '[0-9]' ), `joined_parse`.`value`, NULL)) AS `electrovalvula`,
+    max(if((`joined_parse`.`message` like '%contactor%' and `joined_parse`.`value` not REGEXP '[0-9]'), `joined_parse`.`value`, NULL)) AS `contactor`,
     max(`joined_parse`.`created_at`) AS `emitted_at`
 from
     `joined_parse`
 group by
     `joined_parse`.`house`,
-    minute(`joined_parse`.`created_at`);
+    cast(`joined_parse`.`created_at` as date),
+    minute(`joined_parse`.`created_at`)
+order by
+    `emitted_at`;
+
+
+CREATE TABLE `naturevillage_houses` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `house` varchar(255) DEFAULT NULL,
+  `water_gal` int unsigned DEFAULT NULL,
+  `energy_wh` int unsigned DEFAULT NULL,
+  `wifi_dbm` varchar(10) DEFAULT NULL,
+  `electrovalvula` varchar(255) DEFAULT NULL,
+  `contactor` varchar(255) DEFAULT NULL,
+  `emitted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4423 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE EVENT `sync_houses_table` 
+ON SCHEDULE EVERY 1 MINUTE  
+ DO insert into naturevillage_houses (house, water_gal, energy_wh, wifi_dbm, electrovalvula, contactor, emitted_at)
+   select house, water_gal, energy_wh, wifi_dbm, electrovalvula, contactor, emitted_at 
+   from house
+   where emitted_at > (SELECT emitted_at from naturevillage_houses order by emitted_at desc limit 1);
+
